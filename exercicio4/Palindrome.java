83e5d3b0f07a492ea2bd7f4d446cabe5163a9a03
cacocacaio <ccnsassino1998@hotmail.com>
2016-03-09 15:40:50 -0300

    adicionado o exercicio 4: palindromo

---
+++ exercicio4/Palindrome.java
@@ -0,0 +1,38 @@
+public class Palindrome{
+	
+	public String invertePal(String arg){
+		//Esse método reverte um argumento
+		StringBuffer inverso = new StringBuffer(arg);
+		inverso.reverse();
+		arg = inverso.toString();
+		return arg;
+	}
+
+	public String findPalin(String pal){
+		String inverso = new String(invertePal(pal));
+		//Primeira comparação, antes de começar a fazer uma varredura no argumento
+		if ( pal.equals(inverso) == true ){
+			return pal;
+		}else{
+			//Varredura agora. stringCortada é a uma substring do argumento de entrada, que será usada para fazer a varredura do argumento
+			String stringCortada = new String(pal);
+			for (int i=pal.length(); i>=5; i--){
+				for (int j=0; (j+i) < pal.length(); j++){
+					stringCortada = pal.substring(j, j+i);
+					inverso = invertePal(stringCortada);
+					if ( stringCortada.equals(inverso) == true ){
+						return stringCortada;
+					}else{
+					}
+				}
+			}
+		}
+		return ("Não há palíndromes no argumento inserido");
+	}
+
+	public static void main (String [] args){
+		Palindrome obj = new Palindrome();
+		String palin = obj.findPalin(args[0]);
+		System.out.println("Palíndrome: " + palin);
+	}
+}
